0a8eab2b60264ec8001c5e9e6c4225cfa3db861f
ChandraShekhar <csyadav0513@gmail.com>
2024-09-23 19:58:21 +0530

    Refactor page component: Remove Header component from Home page

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,8 @@
 import type { Metadata } from "next";
 import { Stardos_Stencil } from "next/font/google";
 import "./globals.css";
+import { ThemeProvider } from "@/components/elements/theme-provider";
+import Header from "@/components/elements/Header";
 
 const fredericka = Stardos_Stencil({
   subsets: ["latin"],
@@ -19,9 +21,19 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <html lang="en" suppressHydrationWarning>
       <body className={` ${fredericka.className} antialiased`}>
-        <main className="px-10 h-screen w-full">{children}</main>
+        <ThemeProvider
+          attribute="class"
+          defaultTheme="system"
+          enableSystem
+          disableTransitionOnChange
+        >
+          <main className="px-10 h-screen w-full">
+            <Header />
+            {children}
+          </main>
+        </ThemeProvider>
       </body>
     </html>
   );
--- src/components/elements/Header.tsx
+++ src/components/elements/Header.tsx
@@ -1,17 +1,36 @@
+import Link from "next/link";
 import { Button } from "../ui/button";
 import Logo from "./Logo";
+import { ModeToggle } from "./ModeToggle";
 
 const Header = () => {
   return (
     <header className="flex justify-between items-center px-2 py-2">
       <div className="flex justify-center items-center gap-8">
         <Logo />
-        <p>Leaderboard</p>
-        <p>About us</p>
+        <Link
+          href={"/leaderboard"}
+          className="hover:text-gray-300  cursor-pointer font-medium"
+        >
+          Leaderboard
+        </Link>
+        <Link
+          href={"/about"}
+          className="hover:text-gray-300  cursor-pointer font-medium"
+        >
+          About us
+        </Link>
       </div>
+      {/* Left Side */}
       <div className="flex justify-center items-center gap-3">
-        <Button>Sign In</Button>
-        <Button variant={"ghost"}>Register</Button>
+        <Link href={"login"}>
+          {" "}
+          <Button>Login</Button>
+        </Link>
+        <Link href="register">
+          <Button variant={"ghost"}>Register</Button>
+        </Link>
+        <ModeToggle />
       </div>
     </header>
   );
