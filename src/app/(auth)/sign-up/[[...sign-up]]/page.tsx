a403cfa17550d7f3f887a32b6345e20edd114bd9
ChandraShekhar <csyadav0513@gmail.com>
2024-09-24 13:07:17 +0530

    Refactor sign-up page: Update component styling and image dimensions

--- src/app/(auth)/sign-up/[[...sign-up]]/page.tsx
+++ src/app/(auth)/sign-up/[[...sign-up]]/page.tsx
@@ -4,8 +4,8 @@ import Image from "next/image";
 const page = () => {
   return (
     <>
-      <div className="w-full lg:grid lg:min-h-[600px] lg:grid-cols-2 xl:min-h-[800px]">
-        <div className="flex items-center justify-center py-12">
+      <div className="w-full lg:grid lg:max-h-[500px] lg:grid-cols-2 xl:max-h-[800px]">
+        <div className="flex items-center justify-center ">
           <div className="mx-auto grid w-[350px] gap-6">
             <div>
               <SignUp signInUrl="/sign-in" />
@@ -14,13 +14,11 @@ const page = () => {
         </div>
         <div className="hidden  lg:block">
           <Image
-            placeholder="blur"
-            blurDataURL="blur.jpeg"
             src="/anime-girl-1.jpg"
             alt="Image"
             width="1920"
             height="1080"
-            className="h-full w-full object-cover rounded-3xl    "
+            className="h-full w-full object-cover rounded-2xl dark:brightness-[1.2] "
           />
         </div>
       </div>
