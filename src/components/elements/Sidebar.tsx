1cf918ea301993d27af5c23353b977803c046f53
ChandraShekhar <csyadav0513@gmail.com>
2024-09-25 12:18:35 +0530

    Refactor Sidebar component: Update styles and add current path highlighting

--- src/components/elements/Sidebar.tsx
+++ src/components/elements/Sidebar.tsx
@@ -12,9 +12,13 @@ import {
 import Link from "next/link";
 import { useClerk } from "@clerk/nextjs";
 import { Button } from "../ui/button";
+import { usePathname } from "next/navigation";
 
 const Sidebar = () => {
   const { signOut } = useClerk();
+  const currentPath = usePathname();
+
+  console.log(currentPath);
 
   return (
     <>
@@ -23,59 +27,88 @@ const Sidebar = () => {
         <div className="flex-grow mt-4 px-2 space-y-4">
           <Link
             href="/dashboard"
-            className="flex items-center p-3 dark:text-white text-gray-500 dark:hover:bg-gray-700 hover:bg-gray-100 hover:text-gray-700 transition-all duration-300 rounded"
+            className={`flex items-center p-3 dark:text-white text-gray-500 dark:hover:bg-gray-700 hover:bg-gray-100 hover:text-gray-700 transition-all duration-300 rounded ${
+              currentPath === "/dashboard"
+                ? "bg-gray-100 text-gray-700 dark:bg-gray-700"
+                : ""
+            }`}
           >
             <LayoutDashboard className="h-6 w-6" />
             <span className="ml-4 text-lg">Dashboard</span>
           </Link>
           <Link
             href="/quiz/create-quiz"
-            className="flex items-center p-3 dark:text-white text-gray-500 dark:hover:bg-gray-700 hover:bg-gray-100 hover:text-gray-700 transition-all duration-300 rounded"
+            className={`flex items-center p-3 dark:text-white text-gray-500 dark:hover:bg-gray-700 hover:bg-gray-100 hover:text-gray-700 transition-all duration-300 rounded ${
+              currentPath === "/quiz/create-quiz"
+                ? "bg-gray-100 text-gray-700 dark:bg-gray-700"
+                : ""
+            }`}
           >
             <BadgePlus className="h-6 w-6" />
             <span className="ml-4 text-lg">Create Quiz</span>
           </Link>
           <Link
             href="/quiz/123"
-            className="flex items-center p-3 dark:text-white text-gray-500 dark:hover:bg-gray-700 hover:bg-gray-100 hover:text-gray-700 transition-all duration-300 rounded"
+            className={`flex items-center p-3 dark:text-white text-gray-500 dark:hover:bg-gray-700 hover:bg-gray-100 hover:text-gray-700 transition-all duration-300 rounded ${
+              currentPath === "/quiz/123"
+                ? "bg-gray-100 text-gray-700 dark:bg-gray-700"
+                : ""
+            }`}
           >
             <Swords className="h-6 w-6" />
             <span className="ml-4 text-lg">Room</span>
           </Link>
           <Link
-            href="/quiz/123"
-            className="flex items-center p-3 dark:text-white text-gray-500 dark:hover:bg-gray-700 hover:bg-gray-100 hover:text-gray-700 transition-all duration-300 rounded"
+            href="/leaderboard"
+            className={`flex items-center p-3 dark:text-white text-gray-500 dark:hover:bg-gray-700 hover:bg-gray-100 hover:text-gray-700 transition-all duration-300 rounded ${
+              currentPath === "/leaderboard"
+                ? "bg-gray-100 text-gray-700 dark:bg-gray-700"
+                : ""
+            }`}
           >
             <SquareKanban className="h-6 w-6" />
             <span className="ml-4 text-lg">LeaderBoard</span>
           </Link>
           <Link
             href="/history"
-            className="flex items-center p-3 dark:text-white text-gray-500 dark:hover:bg-gray-700 hover:bg-gray-100 hover:text-gray-700 transition-all duration-300 rounded"
+            className={`flex items-center p-3 dark:text-white text-gray-500 dark:hover:bg-gray-700 hover:bg-gray-100 hover:text-gray-700 transition-all duration-300 rounded ${
+              currentPath === "/history"
+                ? "bg-gray-100 text-gray-700 dark:bg-gray-700"
+                : ""
+            }`}
           >
             <History className="h-6 w-6" />
             <span className="ml-4 text-lg">History</span>
           </Link>
           <Link
             href="/ai"
-            className="flex items-center p-3 dark:text-white text-gray-500 dark:hover:bg-gray-700 hover:bg-gray-100 hover:text-gray-700 transition-all duration-300 rounded"
+            className={`flex items-center p-3 dark:text-white text-gray-500 dark:hover:bg-gray-700 hover:bg-gray-100 hover:text-gray-700 transition-all duration-300 rounded ${
+              currentPath === "/ai"
+                ? "bg-gray-100 text-gray-700 dark:bg-gray-700"
+                : ""
+            }`}
           >
             <Sparkles className="h-6 w-6" />
             <span className="ml-4 text-lg">AI</span>
           </Link>
           <Link
             href="/shop"
-            className="flex items-center p-3 dark:text-white text-gray-500 dark:hover:bg-gray-700 hover:bg-gray-100 hover:text-gray-700 transition-all duration-300 rounded"
+            className={`flex items-center p-3 dark:text-white text-gray-500 dark:hover:bg-gray-700 hover:bg-gray-100 hover:text-gray-700 transition-all duration-300 rounded ${
+              currentPath === "/shop"
+                ? "bg-gray-100 text-gray-700 dark:bg-gray-700"
+                : ""
+            }`}
           >
             <ShoppingBag className="h-6 w-6" />
             <span className="ml-4 text-lg">Shop</span>
           </Link>
         </div>
-        <div className="mt-4 mb-5">
+        <div className="mt-4 mb-5 flex justify-start items-center">
           <Button
             variant={"ghost"}
             onClick={() => signOut()}
-            className="flex items-center p-3 dark:text-white text-gray-500 dark:hover:bg-gray-700 hover:bg-gray-100 hover:text-gray-700 transition-all duration-300 rounded"
+            className={`flex items-center p-3 dark:text-white text-gray-500 w-full  hover:text-gray-700 transition-all duration-300 rounded 
+             `}
           >
             <LogOut className="h-6 w-6" />
             <span className="ml-4 text-lg">Logout</span>
