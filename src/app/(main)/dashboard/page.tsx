a7ac44108f34d9643f712dcb7d0fd85aca345191
ChandraShekhar <csyadav0513@gmail.com>
2024-09-26 12:51:05 +0530

    Refactor dashboard page: Update layout and add heading

--- src/app/(main)/dashboard/page.tsx
+++ src/app/(main)/dashboard/page.tsx
@@ -10,8 +10,13 @@ export const metadata: Metadata = {
 
 const page = () => {
   return (
-    <Dashboard />
-  )
+    <div className="flex justify-center flex-col items-center w-full h-full">
+      <h1 className="text-2xl sm:text-4xl md:text-5xl font-extrabold text-transparent bg-clip-text bg-gradient-to-r from-blue-400 via-pink-500 to-red-500 drop-shadow-lg text-center">
+        Dashboard 
+      </h1>
+      <Dashboard />
+    </div>
+  );
 }
 export default page
 
